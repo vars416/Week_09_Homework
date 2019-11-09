b90542f4cab173212c9ba16a28fb6356b8d61d45
vars416 <56270900+vars416@users.noreply.github.com>
2019-11-09 18:16:03 -0500

    Player Movement Script added

---
+++ Week_09_Assignment/Assets/Scripts/Cube_Movement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube_Movement : MonoBehaviour
+{
+    public float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+    }
+}
